779d8f91621c4e52d69b71a144f7d80cae7f1aaf
Rohit Kumar <kmr.rht.97@gmail.com>
2020-05-28 23:13:42 +0530

    login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginScreenComponent } from './login/login-screen/login-screen.component';
+import { HomeScreenComponent } from './home/home-screen/home-screen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"login",
+    component:LoginScreenComponent
+  },
+  {
+    path:"home",
+    component:HomeScreenComponent
+  },
+  {
+    path:"",
+    redirectTo: "login",
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login-screen/login-screen.component.ts
+++ src/app/login/login-screen/login-screen.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, GoogleLoginProvider, SocialUser } from '../../../../node_modules/angularx-social-login';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.css']
 })
+
 export class LoginScreenComponent implements OnInit {
 
-  constructor() { }
+  private router: Router;
+  private user: SocialUser;
+  loggedIn: Boolean;
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    if(this.loggedIn==true)
+    {
+      this.router.navigateByUrl("http://localhost:4200/home");
+    }
+    this.authService.authState.subscribe((user)=>{
+      this.user = user;
+      this.loggedIn = (user != null);
+      console.log(user);
+    });
   }
-
+  
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  signOut(): void {
+    this.authService.signOut();
+  }
+  putOnConsole(): void {
+    console.log(this.user);
+  }
 }
